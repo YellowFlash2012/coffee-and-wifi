1e64b20ed3e7b4f67242100ec675b234cbd1a968
YellowFlash2012 <cyclop9@hotmail.com>
2021-10-14 07:35:05 +0100

    refactored the cafes.html, finished the add.html and updated the main.py route related to it.

--- main.py
+++ main.py
@@ -1,26 +1,37 @@
-from flask import Flask, render_template
+from os import close
+from flask import Flask, render_template, redirect, url_for
 from flask_bootstrap import Bootstrap
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms.fields.core import SelectField
+from wtforms.validators import URL, DataRequired, InputRequired, InputRequired
 import csv
+from config import *
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SECRET_KEY'] = token
 Bootstrap(app)
 
 
 class CafeForm(FlaskForm):
-    cafe = StringField('Cafe name', validators=[DataRequired()])
+    cafe = StringField('Cafe name', render_kw={'autofocus': True}, validators=[DataRequired()])
+    
+    location = StringField('Cafe Location on Google Maps (URL)', validators=[URL()])
+    
+    open = StringField('Opening @ (e.g 8AM)', validators=[DataRequired()])
+    
+    close = StringField('Closing @ (e.g 5:30PM)', validators=[DataRequired()])
+    
+    coffee = SelectField('Coffee rating', choices=[(
+        '☕️'), ('☕️☕️'), ('☕️☕️☕️'), ('☕️☕️☕️☕️'), ('☕️☕️☕️☕️☕️')], validators=[DataRequired()])
+    
+    wifi = SelectField(
+        'WiFi Strength Rating', choices=[('✘'), ('💪'), ('💪💪'), ('💪💪💪'), ('💪💪💪💪'), ('💪💪💪💪💪')], validators=[DataRequired()])
+    
+    power = SelectField(
+        'Power Socket Availability', choices=[('✘'), ('🔌'), ('🔌🔌'), ('🔌🔌🔌'), ('🔌🔌🔌🔌'), ('🔌🔌🔌🔌🔌')], validators=[DataRequired()])
+    
     submit = SubmitField('Submit')
-
-# Exercise:
-# add: Location URL, open time, closing time, coffee rating, wifi rating, power outlet rating fields
-# make coffee/wifi/power a select element with choice of 0 to 5.
-#e.g. You could use emojis ☕️/💪/✘/🔌
-# make all fields required except submit
-# use a validator to check that the URL field has a URL entered.
-# ---------------------------------------------------------------------------
 
 
 # all Flask routes below
@@ -29,14 +40,22 @@ def home():
     return render_template("index.html")
 
 
-@app.route('/add')
+@app.route('/add', methods=['GET', 'POST'])
 def add_cafe():
     form = CafeForm()
     if form.validate_on_submit():
-        print("True")
-    # Exercise:
-    # Make the form write a new row into cafe-data.csv
-    # with   if form.validate_on_submit()
+        print("True", flush=True)
+        # print(form.data, flush=True)
+        with open('/Users/Cyclo/Documents/full-stack/back-end/100-days-code/day-62-coffee-wifi/coffee-and-wifi/cafe-data.csv', mode="a", encoding="utf8") as file:
+            file.write(f"\n{form.cafe.data},"
+                       f"{form.location.data},"
+                       f"{form.open.data},"
+                       f"{form.close.data},"
+                       f"{form.coffee.data},"
+                       f"{form.wifi.data},"
+                       f"{form.power.data}")
+        return redirect(url_for('cafes')) #to redirect to cafes.html after submit
+    
     return render_template('add.html', form=form)
 
 
@@ -48,7 +67,7 @@ def cafes():
         list_of_rows = []
         for row in csv_data:
             list_of_rows.append(row)
-        print(list_of_rows[0], flush=True)
+            # print(list_of_rows, flush=True)
     return render_template('cafes.html', cafes=list_of_rows)
 
 
